74233cc55cb378500068c5c1e44945a42f16ac8b
siddharthswami23 <siddharth.swami@walchandsangli.ac.in>
2025-10-23 21:50:09 +0530

    anzo-studio a modern portfolio clone

--- microservices-project-code-snippet-app/comments/routes/comments.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const { addComment, getCommentsBySnippetId } = require('../controllers/comments.controller')
-const CommentRouter = express.Router()
-
-CommentRouter.post('/add', addComment)
-CommentRouter.get('/get/:snippetId', getCommentsBySnippetId)
-
-module.exports = CommentRouter
