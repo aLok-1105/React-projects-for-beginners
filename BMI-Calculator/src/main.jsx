b583a51b8c7441e65dae7b6fd7712768b297cad5
Abhiho11a <abhishekholla01@gmail.com>
2025-10-26 17:04:58 +0530

    Improve BMI Calculator: validation, unit toggle, button labels

--- BMI-Calculator/src/main.jsx
+++ BMI-Calculator/src/main.jsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { TolgeeProvider } from "@tolgee/react";
+import { Tolgee, DevTools } from "@tolgee/web";
 
-createRoot(document.getElementById('root')).render(
+const tolgee = Tolgee()
+  .use(DevTools())
+  .init({
+    apiUrl: "https://app.tolgee.io",
+    language: "en",
+    fallbackLanguage: "en",
+  });
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <TolgeeProvider tolgee={tolgee}>
+      <App />
+    </TolgeeProvider>
+  </StrictMode>
+);
